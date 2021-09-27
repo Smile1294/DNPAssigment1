82ee65b08c9b35c4747520942f1a5749329a3255
david <michalcodavid@gmail.com>
2021-09-28 00:47:12 +0200

    Initial Commit

---
+++ DNPAssigment1/tailwind.config.js
@@ -0,0 +1,10 @@
+﻿const colors = require('tailwindcss/colors');
+module.exports = {
+    purge: [],
+    darkMode: false,
+    theme:[],
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+}
